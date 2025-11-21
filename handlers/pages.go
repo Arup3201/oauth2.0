64ea9447bfb707ca87135e1b46748e7221e1cd83
Arup3201 <arupjana7365@gmail.com>
2025-11-21 20:46:19 +0530

    handlers seperated

---
+++ handlers/pages.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func parseExecuteTemplate(templateFile string, w io.Writer, data any) error {
+	file, err := os.ReadFile(templateFile)
+	if err != nil {
+		log.Printf("[ERROR] error opening template file")
+		return err
+	}
+
+	templateFilename := strings.Split(templateFile, ".")
+	tmpl, err := template.New(templateFilename[0] + ".html").Parse(string(file))
+	if err != nil {
+		log.Printf("[ERROR] error parsing template file")
+		return err
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("[ERROR] error parsing template file")
+		return err
+	}
+
+	return nil
+}
+
+func RegisterPage(w http.ResponseWriter, r *http.Request) {
+	parseExecuteTemplate("templates/register.tmpl", w, nil)
+}
+
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+	parseExecuteTemplate("templates/login.tmpl", w, nil)
+}
+
+func ConsentPage(w http.ResponseWriter, r *http.Request) {
+	// extract client data and scopes
+	parseExecuteTemplate("templates/permissions.tmpl", w, nil)
+}
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 	"time"
+
+	"github.com/arup3201/oauth2.0/handlers"
 )
 
 const (
@@ -16,49 +14,12 @@ const (
 	PORT = 8081
 )
 
-func parseExecuteTemplate(templateFile string, w io.Writer, data any) error {
-	file, err := os.ReadFile(templateFile)
-	if err != nil {
-		log.Printf("[ERROR] error opening template file")
-		return err
-	}
-
-	templateFilename := strings.Split(templateFile, ".")
-	tmpl, err := template.New(templateFilename[0] + ".html").Parse(string(file))
-	if err != nil {
-		log.Printf("[ERROR] error parsing template file")
-		return err
-	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		log.Printf("[ERROR] error parsing template file")
-		return err
-	}
-
-	return nil
-}
-
-func registerPage(w http.ResponseWriter, r *http.Request) {
-	parseExecuteTemplate("templates/register.tmpl", w, nil)
-}
-
-func loginPage(w http.ResponseWriter, r *http.Request) {
-	parseExecuteTemplate("templates/login.tmpl", w, nil)
-}
-
-func consentPage(w http.ResponseWriter, r *http.Request) {
-	// extract client data and scopes
-	parseExecuteTemplate("templates/permissions.tmpl", w, nil)
-}
-
 func main() {
-
 	mux := http.NewServeMux()
 	mux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
-	mux.HandleFunc("GET /register-page", registerPage)
-	mux.HandleFunc("GET /login-page", loginPage)
-	mux.HandleFunc("GET /consent-page", consentPage)
+	mux.HandleFunc("GET /register-page", handlers.RegisterPage)
+	mux.HandleFunc("GET /login-page", handlers.LoginPage)
+	mux.HandleFunc("GET /consent-page", handlers.ConsentPage)
 
 	server := &http.Server{
 		Addr:         fmt.Sprintf("%s:%d", HOST, PORT),
